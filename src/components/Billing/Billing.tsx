150e2af866d8f213b437bced18fd77741e8ee0c6
sayvai <prakash@sayvai.io>
2024-07-10 13:47:56 +0530

    Billing changes

--- src/components/Billing/Billing.tsx
+++ src/components/Billing/Billing.tsx
@@ -7,7 +7,9 @@ const Billing: React.FC = () => {
   return (
     <div className="z-50 col-span-12 rounded-tl-2xl h-screen rounded-tr-2xl bg-white px-5 pt-2 dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:col-span-9">
       <div className="w-full gap-3 mt-4 sm:gap-5">
-        <h1 className="text-3xl text-black font-bold">Webhook Settings</h1>
+        <h1 className="text-3xl text-black font-bold">Billing
+          
+        </h1>
         <h2 className="text-md mt-4">
         Configure your webhook URL to receive real-time event notifications.
         </h2>
